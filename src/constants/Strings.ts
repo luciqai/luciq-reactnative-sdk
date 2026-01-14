753d92683e6bc23c90b9d3f1f0733184d5bb772c
Andrew Daniel <adaniel@luciq.ai>
2026-01-14 18:30:58 +0200

    update error logs msgs

--- src/constants/Strings.ts
+++ src/constants/Strings.ts
@@ -1,15 +1,15 @@
 export class LuciqStrings {
   static readonly customSpanAPMDisabledMessage: string =
     'APM is disabled, custom span not created. Please enable APM by following the instructions at this link:\n' +
-    'https://docs.luciq.ai/reference#showing-and-manipulating-the-invocation';
+    'https://docs.luciq.ai/product-guides-and-integrations/product-guides/application-performance-monitoring';
 
   static readonly customSpanDisabled: string =
     'Custom span is disabled, custom span not created. Please enable Custom Span by following the instructions at this link:\n' +
-    'https://docs.luciq.ai/reference#showing-and-manipulating-the-invocation';
+    'https://docs.luciq.ai/product-guides-and-integrations/product-guides/application-performance-monitoring';
 
   static readonly customSpanSDKNotInitializedMessage: string =
     'Luciq API was called before the SDK is built. To build it, first by following the instructions at this link:\n' +
-    'https://docs.luciq.ai/reference#showing-and-manipulating-the-invocation';
+    'https://docs.luciq.ai/product-guides-and-integrations/product-guides/application-performance-monitoring';
 
   static readonly customSpanNameEmpty: string =
     'Custom span name cannot be empty. Please provide a valid name for the custom span.';
